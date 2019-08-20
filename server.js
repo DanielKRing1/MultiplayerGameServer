6aacddfb99bae77368a20625475d8627f8df0a00
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-19 22:48:30 -0700

    Added express app to initialize socket

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello I this is my personal website. Ill be adding a udp server soon'));
+app.get('/', (req, res) => res.send('Hello this is my personal website. Ill be adding a udp server soon'));
 
-app.listen(PORT, () => console.log('DanielRing.gq is running'));
+app.listen(PORT, () => console.log(`DanielRing.gq is running on port ${PORT}`));
