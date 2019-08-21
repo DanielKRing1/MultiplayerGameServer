6f04fd6bff1f8c27b35fc93151070e2ce8575932
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-20 23:02:58 -0700

    Changed init-connection route back to Post

--- routes/game.js
+++ routes/game.js
@@ -3,5 +3,5 @@ const gameCtrl = require('../controllers/gameCtrl');
 module.exports = (router) => {
     router
         .route('/init-connection')
-        .put(gameCtrl.addPlayer);
+        .post(gameCtrl.addPlayer);
 }
