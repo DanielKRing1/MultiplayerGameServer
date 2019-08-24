9510f3548c5eef7fd7ca2fb411f9302e1af989cd
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-24 01:21:43 -0700

    Moved to separate folder

---
+++ Redis/players.js
@@ -0,0 +1,5 @@
+const players = require('./redis').getClient();
+
+module.exports = {
+    
+}
---
+++ Redis/redis.js
@@ -0,0 +1,16 @@
+const redis = require('redis');
+const Promise = require('bluebird');
+
+let client;
+
+module.exports = {
+    init: () => {
+        client = Promise.promisifyAll(redis.createClient());
+
+        client.on('connect', () => console.log('Redis connected!') );
+
+        client.on('error', (err) => console.log(`Redis error: ${err}`) );
+    },
+
+    getClient: () => client
+}
