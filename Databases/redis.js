a77b3b1c226f196cd5a517f8258b7b501cf1f034
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-23 17:13:10 -0700

    Added Bluebird package to support async/ await, instead of promises

--- Databases/redis.js
+++ Databases/redis.js
@@ -1,10 +1,11 @@
 const redis = require('redis');
+const Promise = require('bluebird');
 
 let client;
 
 module.exports = {
     init: () => {
-        client = redis.createClient();
+        client = Promise.promisifyAll(redis.createClient());
 
         client.on('connect', () => console.log('Redis connected!') );
 
