3a869eaa3ece887903a5d71b18900bc8c761b03b
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-26 15:10:49 -0700

    Added direction vector

--- GameElements/Player.js
+++ GameElements/Player.js
@@ -1,10 +1,14 @@
 const Circle = require('./Circle');
 
 const Player = class extends Circle {
-    constructor(id, xPos, yPos, radius, team) {
+    constructor(id, xPos, yPos, xDir, yDir, radius, team) {
         super(id, xPos, yPos, radius);
 
         this.team = team;
+        this.direction = {
+            x: xDir,
+            y: yDir
+        };
     }
 }
 
