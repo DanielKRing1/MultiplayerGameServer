e38d337afc5dcf63dc132f5ac501f50caf430d03
DanielKRing1 <DanielKRing1@gmail.com>
2019-08-26 15:13:08 -0700

    Added normalizeVector()

--- util/methods.js
+++ util/methods.js
@@ -1,5 +1,14 @@
 module.exports = {
     randInt: (low, high) => {
         return low + Math.floor(Math.random() * (high - low + 1));
+    },
+    normalizeVector: (vector) => {
+        const magnitude = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+        const unitVector = {
+            x: vector.x / magnitude,
+            y: vector.y / magnitude
+        };
+    
+        return unitVector;
     }
 }
