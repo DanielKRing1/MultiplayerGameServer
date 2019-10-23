c7a22f16a77350dc18a08ed64a6ab8b76f94e71d
DanielKRing1 <DanielKRing1@gmail.com>
2019-10-22 17:24:48 -0700

    Starting work on self updating GameEngine

--- Game/GameEngine.js
+++ Game/GameEngine.js
@@ -56,6 +56,8 @@ const updatePlayerPositions = (players) => {
     const elapsedTime = now - lastUpdate;
     lastUpdate = now;
 
+    console.log(players);
+
     players.forEach(player => {
         player.updatePosition(elapsedTime)
     });
