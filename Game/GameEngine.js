a57644ddbb5783063e934d5185600bce97f5e68c
DanielKRing1 <DanielKRing1@gmail.com>
2019-10-22 17:37:24 -0700

    Starting work on self updating GameEngine

--- Game/GameEngine.js
+++ Game/GameEngine.js
@@ -56,11 +56,7 @@ const updatePlayerPositions = (players) => {
     const elapsedTime = now - lastUpdate;
     lastUpdate = now;
 
-    console.log(1);
     Object.keys(players).forEach(key => {
-    console.log(2);
-
-        console.log(key);
         const player = players[key];
         console.log(player);
         player.updatePosition(elapsedTime)
@@ -69,7 +65,6 @@ const updatePlayerPositions = (players) => {
 const sendBoardToClients = (players) => {
     // const bufferData = Buffer.from(this.board);
 
-    console.log(3);
     Object.keys(players).forEach(key => {
         const player = players[key];
         sendMessage(player.ip, player.port, this.board);
