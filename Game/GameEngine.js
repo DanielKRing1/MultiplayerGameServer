565d001990ce30ea0908e5788bca118894619b4f
DanielKRing1 <DanielKRing1@gmail.com>
2019-10-24 13:53:36 -0700

    Fixed elapsedTime calculation

--- Game/GameEngine.js
+++ Game/GameEngine.js
@@ -56,7 +56,6 @@ const updatePlayerPositions = (players) => {
     const elapsedTime = now - lastUpdate;
     lastUpdate = now;
 
-    console.log(elapsedTime)
 
     Object.keys(players).forEach(key => {
         const player = players[key];
