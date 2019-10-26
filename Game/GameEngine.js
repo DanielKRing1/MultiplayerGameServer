990f2025f8dd57e0162f0523bb4a57b017c9391b
DanielKRing1 <DanielKRing1@gmail.com>
2019-10-26 02:04:47 -0700

    Added Player Direction Update and Generic Player Update Functionality

--- Game/GameEngine.js
+++ Game/GameEngine.js
@@ -61,7 +61,7 @@ const updatePlayerPositions = (players) => {
     lastUpdate = now;
     
     Object.keys(players).forEach(key => {
-        console.log(lastUpdate);
+        console.log(elapsedTime);
         const player = players[key];
         player.updatePosition(elapsedTime)
     });
